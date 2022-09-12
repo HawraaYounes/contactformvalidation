3d5369c0d5356875bfe69e8675322ce9ba643b24
HawraaYounes <hawraayounes0@gmail.com>
2022-09-12 21:53:11 +0300

    Adding html and defining variables in js file

---
+++ main.js
@@ -0,0 +1,11 @@
+var fullname=document.getElementById("fullname");
+var email=document.getElementById("email");
+var phone=document.getElementById("phone");
+var message=document.getElementById("message");
+var submitBtn=document.getElementById("submit-btn");
+
+submitBtn.addEventListener("click".validateForm);
+
+function validateForm(){
+    
+}
